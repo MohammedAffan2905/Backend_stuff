ff6057fee73c30967ad065348d090fd5e573f364
Mohammed Affan <mohammedaffan2905@gmail.com>
2024-06-20 18:32:43 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 // app.use(Affanmiddleware("affu"))
 app.use('/Affan',Affanapi);
 
-mongoose.connect('mongodb+srv://miniproject1729:miniproject1729@cluster0.ylskpxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',  { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://affan2905:affan2905@cluster0.ncafivz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',  { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Could not connect to MongoDB...', err));
 
